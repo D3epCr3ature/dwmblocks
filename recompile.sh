9ac33aa03b4a647f309e1c4ddcdc0c86f31b7a4a
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-11-04 21:49:46 +0100

    Add test if exist before removing config.h

--- recompile.sh
+++ recompile.sh
@@ -1,6 +1,8 @@
 echo "-------------------------------------------------"
-#echo "Remove old generated config.h"
-#rm config.h
+if [ -f blocks.h ]; then
+	echo "Remove old generated config.h"
+	rm blocks.h
+fi
 echo
 echo "-------------------------------------------------"
 echo "Clean, (Re-)generate config.h and install anew"
