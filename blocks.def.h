139e4354f8e1f392df43f8444df0c456bd154438
D3epCr3ature <kcs.bp@ikmail.com>
2025-07-26 01:31:13 +0200

    Add battery percentage level

--- blocks.def.h
+++ blocks.def.h
@@ -4,6 +4,7 @@ static const Block blocks[] = {
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "echo $(cat /sys/class/power_supply/BAT0/capacity)%",		5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
